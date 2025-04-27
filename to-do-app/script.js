094bfd0b963100bae0c7e6a1e7be74ae316cbfc3
kumar <jitentui417@gmail.com>
2025-04-27 23:31:32 +0900

    new feature list count, add list, delete list

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -1,12 +1,49 @@
-
+// adding to do list on click
 document.getElementById("add-task-btn").addEventListener("click", function () {
     const todoList = document.getElementById('task-input').value;
+    var taskCount = "";
 
     if (todoList) {
+        // add task list
         const li = document.createElement('li');
         li.textContent = todoList;
         document.getElementById('task-list').appendChild(li);
+
+        // count task list and set
+        taskCount = document.querySelectorAll('#task-list li').length; //get list length
+        document.getElementById('count-task').innerHTML = `Total Tasks: ${taskCount}`; // set total task
     } else {
-        alert("Please enter task details.")
+        alert("Please enter task details.");
     }
 });
+
+// delete onClick
+document.getElementById('delete-all-btn').addEventListener('click', function () {
+    document.getElementById('confirmModal').style.display = 'block';
+
+});
+
+// Confirm "Yes"
+document.getElementById('confirmYes').onclick = function () {
+
+    // get the values of list
+    const values = document.querySelectorAll('#task-list li').length; // get length 
+
+
+    if (values) {
+        // remove all ul li
+        const list = document.getElementById('task-list');
+        list.innerHTML = ''; // remove all list
+
+        // task count
+        const totalTask = document.getElementById('count-task');
+        totalTask.innerHTML = "Total Tasks: 0";
+    }
+    document.getElementById('confirmModal').style.display = 'none';// hide confirm model
+};
+
+// Confirm "Cancel"
+document.getElementById('confirmNo').onclick = function () {
+    document.getElementById('confirmModal').style.display = 'none';
+};
+
